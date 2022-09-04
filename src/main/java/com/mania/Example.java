d2ee9389281318620bebc6a50a72058fdd6d9219
sivajiyelugoti <sivaji.y@gmail.com>
2022-09-04 20:54:49 +0100

    added java class files

--- src/main/java/com/mania/Example.java
+++ src/main/java/com/mania/Example.java
@@ -1,5 +1,9 @@
 package com.mania;
 
+
+import javax.validation.constraints.NotNull;
+
+
 @FunctionalInterface
 interface MyInterface{
     Hello display(String say,String hi);
@@ -15,10 +19,28 @@ class Hello{
     }
 
 }
+
+class NullCheck {
+
+    private String employeeId;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(@NotNull final String employeeId) {
+        this.employeeId = employeeId;
+    }
+}
 public class Example {
     public static void main(String[] args) {
         //Method reference to a constructor
         MyInterface ref = Hello::new;
         ref.display("Hello World!","Java");
+
+        NullCheck check = new NullCheck();
+        check.setEmployeeId(null);
+        System.out.println(check.getEmployeeId());
+
     }
 }
--- src/main/java/com/mania/TestClass.java
+++ src/main/java/com/mania/TestClass.java
@@ -1,9 +1,6 @@
 package com.mania;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TestClass {
